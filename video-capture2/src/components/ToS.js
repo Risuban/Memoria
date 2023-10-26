87c8e499ffa0163a3c8d2178faaa7d7578f57304
Risuban <nicotapiamunoz@gmail.com>
2023-10-25 22:03:01 -0300

    directories finally work as intended LET´S GOOOOOO

--- video-capture2/src/components/ToS.js
+++ video-capture2/src/components/ToS.js
@@ -1,80 +1,109 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
+import consentimientoPDF from '../assets/Consentimiento_informado.pdf';
+import { useUser } from './UserContext';
 
 function TermsOfService() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        // ... any other fields you need
-    });
+    const [hasReadDocument, setHasReadDocument] = useState(false);
 
+    const { formData, setFormData } = useUser();
     const navigate = useNavigate(); // se utiliza aquí
+    const lastName = formData.lastName
+    const apellido = lastName.split(` `).join(`-`)
+    
+
 
     const handleInputChange = (event) => {
         const { name, value } = event.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
+        const updatedTimestamp = Date.now();
+        setFormData(prevState => ({ ...prevState, [name]: value, timeStamp: updatedTimestamp }));
+        console.log(formData);
+        
     };
 
     const handleSubmit = (event) => {
-        console.log("Submit event triggered"); // Verificar si esto se muestra en la consola cuando haces clic en "Submit"
         event.preventDefault();
-        // llamada a la API para crear el directorio
-        axios.post('http://localhost:3001/create-user-directory', { 
-            userName: formData.name 
-        }, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
+        if (!hasReadDocument) {
+            alert("Por favor, lee y acepta el consentimiento informado.");
+            return;
+        }
+
+        console.log("Enviando datos al servidor...");
+        
+        axios.post('http://localhost:3001/create-user-directory', {
+            ...formData, 
+            userName: formData.firstName + "-" + apellido
         })
         .then(response => {
-            console.log(response.data.message);
+            console.log("Respuesta del servidor:", response.data);
             // Redirige al usuario a la página de grabación
-            navigate('/camera-capture'); // redirige a la ruta de captura de cámara
+            navigate('/camera-capture');
         })
         .catch(error => {
-            console.error("Error en la llamada a la API", error);
+            console.error("Error al comunicarse con el servidor:", error);
         });
+        
     };
 
     return (
         <div className="container mt-5">
             <h2>Terms of Service</h2>
-            {/* Display your terms of service content here */}
             <div className="mt-4">
                 <h3>Enter your details:</h3>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-3">
-                        <label htmlFor="name" className="form-label">Name:</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            className="form-control"
-                            value={formData.name}
-                            onChange={handleInputChange}
-                            required
-                        />
+                        <label htmlFor="firstName" className="form-label">Nombre:</label>
+                        <input type="text" id="firstName" name="firstName" className="form-control" value={formData.firstName} onChange={handleInputChange} placeholder="Nombre"/>
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            className="form-control"
-                            value={formData.email}
-                            onChange={handleInputChange}
-                            required
-                        />
+                        <label htmlFor="lastName" className="form-label">Apellido:</label>
+                        <input type="text" id="lastName" name="lastName" className="form-control" value={formData.lastName} onChange={handleInputChange} placeholder="Apellido"/>
                     </div>
-                    {/* Add other fields as needed */}
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <div className="mb-3">
+                        <label htmlFor="age" className="form-label">Edad:</label>
+                        <input type="number" id="age" name="age" className="form-control" value={formData.age} onChange={handleInputChange} placeholder="Edad"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="gender" className="form-label">Sexo:</label>
+                        <select id="gender" name="gender" className="form-control" value={formData.gender} onChange={handleInputChange}>
+                            <option value="" disabled>Seleccione una opción</option>
+                            <option value="male">Masculino</option>
+                            <option value="female">Femenino</option>
+                        </select>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="underlyingCondition" className="form-label">Enfermedad de base:</label>
+                        <input type="text" id="underlyingCondition" name="underlyingCondition" className="form-control" value={formData.underlyingCondition} onChange={handleInputChange} placeholder="Diabetes, Hipertensión, etc. o 'No aplica'"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="estimatedWeight" className="form-label">Peso estimado (kg):</label>
+                        <input type="number" id="estimatedWeight" name="estimatedWeight" className="form-control" value={formData.estimatedWeight} onChange={handleInputChange} placeholder="Peso en kg"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="height" className="form-label">Altura (cm):</label>
+                        <input type="number" id="height" name="height" className="form-control" value={formData.height} onChange={handleInputChange} placeholder="Altura en cm"/>
+                    </div>
+                    {/* ... tus otros campos ... */}
+                                     {/* Visualizador de PDF */}
+            <iframe src={consentimientoPDF} width="100%" height="1000px"></iframe>
+            {/* Checkbox de consentimiento */}
+            <div className="form-check mt-4">
+                <input 
+                    className="form-check-input" 
+                    type="checkbox" 
+                    value={hasReadDocument} 
+                    onChange={() => setHasReadDocument(!hasReadDocument)} 
+                    id="consentCheck"
+                />
+                <label className="form-check-label" htmlFor="consentCheck">
+                    He leído el documento y doy mi consentimiento.
+                </label>
+            </div>
+            <button type="submit" className="btn btn-primary mt-3" disabled={!hasReadDocument}>Submit</button>
+
                 </form>
+
             </div>
         </div>
     );
--- video-capture2/src/services/uploadVideo.js
+++ video-capture2/src/services/uploadVideo.js
@@ -1,11 +1,24 @@
 // Dentro de src/utils/uploadVideo.js
 import axios from 'axios';
+import { json } from 'react-router-dom';
 
-const uploadVideo = (videoBlob, filename) => {
-  const formData = new FormData();
-  formData.append('video', videoBlob, filename);
+const uploadVideo = (videoBlob, filename, formData) => {
+  const uploadData = new FormData();
+  uploadData.append('userInfo', JSON.stringify(formData));
+  uploadData.append('video', videoBlob, filename);
+  
+  console.log(videoBlob);  // Deberías ver el objeto Blob del video
+  console.log(filename);   // Deberías ver el nombre del archivo que esperas
+  console.log(formData);  // Deberías ver el objeto con la información del usuario
+  const formDataString = JSON.stringify(formData);
+  console.log(formDataString);  // Deberías ver la representación en cadena de la información del usuario
+  uploadData.forEach((value, key) => {
+    console.log(key, value);
+  });
+    
+      // Aquí pasamos formData como userInfo
 
-  return axios.post('http://localhost:3001/upload', formData, {
+  return axios.post('http://localhost:3001/upload', uploadData, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
